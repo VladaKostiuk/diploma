3b2249f3904e8508845e24158f0cd407bfaf27d8
Hlieb Shchehelskyi <hlieb.shchehelskyi@vilmate.com>
2023-05-20 21:26:37 +0300

    initialize project

--- types/map.d.ts
+++
@@ -1,7 +0,0 @@
-export type GoogleMapType = google.maps.Map;
-export type GoogleMapPolygon = google.maps.Polygon;
-export type GoogleMapDrawingManager = google.maps.drawing.DrawingManager;
-export type GoogleMapRectangle = google.maps.Rectangle;
-export type GoogleMapCircle = google.maps.Circle;
-export type GoogleMapOverlayType = google.drawing.OverlayType;
-export type GoogleMapPolygonOptions = google.maps.PolygonOptions;
