d5f25fad0b915fc5880327e5d8a6eeb7576f0529
Hlieb Shchehelskyi <hlieb.shchehelskyi@vilmate.com>
2023-05-27 03:08:08 +0300

    create dummy filters

--- src/components/app/App/index.tsx
+++ src/components/app/App/index.tsx
@@ -6,6 +6,7 @@ import {
   List,
   ListItem,
   Paper,
+  TextField,
   Typography,
 } from '@mui/material';
 import { ClockGroup } from 'components/unsorted/ClockGroup';
@@ -64,13 +65,13 @@ export const App: FC = () => {
   };
 
   const handleGetDbData = () => {
-    // localforage.getItem(Stores.Customers).then((dbCustomers) => {
-    //   setDbCustomers(dbCustomers as PreparedCustomersData);
-    // });
+    localforage.getItem(Stores.Customers).then((dbCustomers) => {
+      setDbCustomers(dbCustomers as PreparedCustomersData);
+    });
 
     // Dummy:
-    const preparedCustomers = prepareCustomersData(dummyCustomers);
-    setDbCustomers(preparedCustomers);
+    // const preparedCustomers = prepareCustomersData(dummyCustomers);
+    // setDbCustomers(preparedCustomers);
   };
 
   const handleGenerateData = () => {
@@ -231,7 +232,71 @@ export const App: FC = () => {
             ))}
           </Box>
         </Box>
-        <Paper></Paper>
+        <Paper sx={{ p: '12px' }}>
+          <Box>
+            <Typography
+              variant="h5"
+              sx={{
+                fontWeight: 'bold',
+                textDecoration: 'underline',
+                mb: '12px',
+              }}
+            >
+              Загальні фільтри:
+            </Typography>
+
+            <Box sx={{ display: 'flex', gap: '8px', mb: '24px' }}>
+              <TextField
+                label="Максимальна кількість кас"
+                size="small"
+                value={1}
+                disabled
+              />
+              <TextField
+                label="Максимальний час обслуговування"
+                size="small"
+                value="-"
+                disabled
+              />
+            </Box>
+            <Typography
+              variant="h5"
+              sx={{
+                fontWeight: 'bold',
+                textDecoration: 'underline',
+                mb: '12px',
+              }}
+            >
+              Фільтри каси №1:
+            </Typography>
+            <Box sx={{ display: 'flex', gap: '8px', mb: '24px' }}>
+              <TextField
+                label="Стан каси"
+                size="small"
+                value="Відкрита"
+                disabled
+              />
+              <TextField
+                label="Час обробки одиниці товару"
+                size="small"
+                value={1}
+                disabled
+              />
+              <TextField
+                label="Максимальна кількість товарів на 1 людину"
+                size="small"
+                value="-"
+                disabled
+              />
+            </Box>
+            <Button variant="contained" color="info">
+              Відкрити касу
+            </Button>
+            <Button sx={{ml: '8px'}} variant="contained" color="warning">
+              Закрити касу
+            </Button>
+          </Box>
+        </Paper>
       </Layout>
 
       <Modal
